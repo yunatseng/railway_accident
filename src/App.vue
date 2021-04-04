645c42f65be671fbf051e50948dfb1bd2e39e703
yunaTseng <fav0124@gmail.com>
2021-04-05 01:10:22 +0800

    ADD: 一些小細節優化、deploy 檔

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
       dark
     >
       <h3 class="ml-2 title font-weight-light">
-        事故時間軸
+        臺鐵事故時間軸
       </h3>
 
       <v-spacer />
@@ -21,7 +21,7 @@
     </v-app-bar>
 
     <v-main>
-      <Main />
+      <Main @clickEventCitation="setFocus" />
     </v-main>
     <template>
       <v-footer
@@ -42,11 +42,13 @@
                 :key="i"
               >
                 <a
-                  class="white--text"
-                  :href="citation"
+                  :id="`event_${i}`"
+                  :tabindex="i"
+                  class="white--text footer-citation"
+                  :href="citation.url"
                   target="_blank"
                 >
-                  {{ citation }}
+                  {{ citation.name }}
                 </a>
               </li>
             </ol>
@@ -57,7 +59,7 @@
           <v-card-text
             class="white--text text-center"
           >
-            {{ new Date().getFullYear() }} —
+            © {{ new Date().getFullYear() }} — Yuna
             <a
               href="mailto:fav0124@gmail.com"
               target="_blank"
@@ -79,18 +81,27 @@ export default {
   components: {
     Main,
   },
-
   data: () => ({
-    citeList: ['https://www.railway.gov.tw/tra-tip-web/adr/about-public-info-6-2?selectIndex=1',
-      'https://www.chinatimes.com/newspapers/20200428000636-260107?chdtv',
-      'https://www.railway.gov.tw/tra-tip-web/adr/about-public-info-6-2?selectIndex=1',
-      'https://www.railway.gov.tw/tra-tip-web/adr/about-public-info-6-2?selectIndex=1',
-      'https://zh.wikipedia.org/wiki/2020%E5%B9%B4%E8%87%BA%E9%90%B5%E7%91%9E%E8%8A%B3%E7%8C%B4%E7%A1%90%E8%B7%AF%E6%AE%B5%E8%B5%B0%E5%B1%B1%E4%BA%8B%E6%95%85',
-      'https://www.railway.gov.tw/tra-tip-web/adr/about-public-info-6-2?selectIndex=1',
-      'https://udn.com/news/story/7266/5208604',
-      'https://www.cna.com.tw/news/asoc/202101190278.aspx',
-      'https://www.motc.gov.tw/ch/home.jsp?id=2&parentpath=0&mcustomize=news_view.jsp&dataserno=202102230005&aplistdn=ou=data,ou=news,ou=chinese,ou=ap_root,o=motc,c=tw&toolsflag=Y&imgfolder=img%2Fstandard',
-      'https://zh.wikipedia.org/wiki/2021%E5%B9%B4%E5%A4%AA%E9%AD%AF%E9%96%A3%E8%99%9F%E5%88%97%E8%BB%8A%E5%87%BA%E8%BB%8C%E4%BA%8B%E6%95%85'],
+    citeList: [{ url: 'https://www.railway.gov.tw/tra-tip-web/adr/about-public-info-6-2?selectIndex=1', name: '交通部臺灣鐵路管理局109年事故資料' },
+      { url: 'https://www.chinatimes.com/newspapers/20200428000636-260107?chdtv', name: '花蓮火車事故 7台鐵員工受傷' },
+      { url: 'https://www.railway.gov.tw/tra-tip-web/adr/about-public-info-6-2?selectIndex=1', name: '交通部臺灣鐵路管理局109年事故資料' },
+      { url: 'https://www.railway.gov.tw/tra-tip-web/adr/about-public-info-6-2?selectIndex=1', name: '交通部臺灣鐵路管理局109年事故資料' },
+      { url: 'https://zh.wikipedia.org/wiki/2020%E5%B9%B4%E8%87%BA%E9%90%B5%E7%91%9E%E8%8A%B3%E7%8C%B4%E7%A1%90%E8%B7%AF%E6%AE%B5%E8%B5%B0%E5%B1%B1%E4%BA%8B%E6%95%85', name: '2020年臺鐵瑞芳猴硐路段走山事故 wiki' },
+      { url: 'https://www.railway.gov.tw/tra-tip-web/adr/about-public-info-6-2?selectIndex=1', name: '交通部臺灣鐵路管理局109年事故資料' },
+      { url: 'https://udn.com/news/story/7266/5208604', name: '調車員遭兩車廂重夾出血身亡 產工指台鐵三肇因釀殉職' },
+      { url: 'https://www.cna.com.tw/news/asoc/202101190278.aspx', name: '疑誤觸高壓電 台南新市火車站2工人受傷送醫' },
+      { url: 'https://www.motc.gov.tw/ch/home.jsp?id=2&parentpath=0&mcustomize=news_view.jsp&dataserno=202102230005&aplistdn=ou=data,ou=news,ou=chinese,ou=ap_root,o=motc,c=tw&toolsflag=Y&imgfolder=img%2Fstandard', name: '臺鐵局2月23日海端站職安事件說明新聞稿' },
+      { url: 'https://zh.wikipedia.org/wiki/2021%E5%B9%B4%E5%A4%AA%E9%AD%AF%E9%96%A3%E8%99%9F%E5%88%97%E8%BB%8A%E5%87%BA%E8%BB%8C%E4%BA%8B%E6%95%85', name: '2021年太魯閣號列車出軌事故 wiki' }],
   }),
+  methods: {
+    setFocus(id) {
+      document.getElementById(`event_${id - 1}`).focus();
+    },
+  },
 };
 </script>
+<style>
+.footer-citation:focus{
+color: peachpuff !important;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,7 @@ module.exports = {
   transpileDependencies: [
     'vuetify',
   ],
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/railway_accident/'
+    : '/',
 };
